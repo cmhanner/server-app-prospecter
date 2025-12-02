d5b711493dd9ebeb0650b35d9eba83f120d40406
cmhanner <chanceforcode@gmail.com>
2025-12-02 12:26:09 -0500

    added error message for debugging

--- server.js
+++ server.js
@@ -160,6 +160,10 @@ const App = mongoose.model("App", appScheme);
 
 //     }
 // ]
+app.use ((req, res, next) => {
+    console.log("Incoming:", req.method, req.url);
+    next();
+})
 
 app.get("/api/apps/", async (req, res) => {
     console.log("In Get Request for all Apps.");
